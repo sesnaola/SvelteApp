9f7d788699f5e39f5afc70b587d28c146ae1de45
Sergi Esnaola <sergiesnaola@gmail.com>
2022-05-28 21:19:42 +0200

    Component with state management

---
+++ my-svelte-project/src/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let initialCounter = 0;
+    let count = initialCounter;
+
+    function handleCount() {
+        count++;
+    }
+</script>
+
+<main>
+    <p>Counter: {count}</p>
+</main>
+
+<button on:click={handleCount}>Add count</button>
